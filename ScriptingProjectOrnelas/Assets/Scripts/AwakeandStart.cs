a90fd4e59f323d365d40d4f6de545231d4759d09
OdelBecham <116104866+OdelBecham@users.noreply.github.com>
2022-11-14 08:49:15 -0800

    Awake and Start script + Scene

---
+++ ScriptingProjectOrnelas/Assets/Scripts/AwakeandStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
